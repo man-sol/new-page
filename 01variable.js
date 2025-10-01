055d48fb8115e9b0d7adf518716a17b54be43506
Manish kumar <manishnkb7@gmail.com>
2025-10-02 03:15:35 +0530

    first commit

---
+++ 01variable.js
@@ -0,0 +1,107 @@
+// let a = Symbol("hero")
+// let b = Symbol("hero")
+// console.log(a == b)
+
+// for(let i = 0; i <= 10; i++){
+//     console.log(i);
+    
+// }
+
+
+// let a = 10;
+
+// while( a >= 1){
+//     console.log(a)
+//     a--
+// }
+//     console.log(a)
+
+
+//  let a = 10;
+
+// do{
+
+//     console.log(a)
+//          a--
+
+// }
+// while(a >= 1)
+
+// let a = [1,2,3,4,5,6]
+
+// for(i of a){
+//     console.log(i)
+// }
+
+// var a = 1
+// var a = 2
+// a = 3
+
+// let b = 1
+// let b = 2
+// b = 3
+
+// const c = 1
+// const c = 2
+// c = 3
+
+// console.log(a)
+// var a = 10
+// sam()
+
+// function sam(){
+// console.log("sam")
+
+
+// }
+
+// function a (){
+
+//     let i = 0;
+
+//     return function(){
+//         console.log(i)
+//         i++
+//     }
+// }
+
+// let ret = a()
+
+
+// ret()
+// ret()
+// ret()
+
+// function sam(){
+
+// }
+
+// let out = new sam()
+
+// console.log(out)
+
+// console.log(0.1 + 0.2)
+// console.log(1<2<3);
+
+
+
+
+
+class Person{
+    constructor(a,b,c,d){
+        this.naam = a;
+        this.umar = b;
+        this.rang = c;
+        this.isMale = d;
+
+    }
+
+ greet ( ){
+        console.log("good Evening...")
+    }
+
+}
+
+let p1 = new Person("Sam", 11, "red", true)
+
+console.log(p1)
---
+++ h.js
@@ -0,0 +1,67 @@
+// function c (){
+//     var a = 1
+//     let b = 2
+// }
+// console.log(a)
+// console.log(b)
+
+
+
+// let nums = [5, 10, 15, 20];
+// let bigNums = nums.filter(function(item, index) {
+//     if(item > 10){
+//         return item
+//     }
+// });
+// console.log(bigNums); // [15, 20]
+
+
+// class person {
+//     constructor(a,b){
+//         this.name = a;
+//         this.age = b;
+//     }
+
+//     greet(){
+//         console.log(`${this.name } age is ${this.age}`)
+//     }
+// }
+
+// let p = new person("manish", 23)
+
+// console.log(p)
+
+
+// class student extends person{
+//     constructor(a,b, rollno){
+//         super(a,b)
+//         this.rollno = rollno;
+
+//     }
+
+//     r(){console.log(`his roll no is ${this.rollno}`)}
+// }
+
+// let s = new student("man", 23, 28)
+
+// console.log(s)
+// s.r()
+
+// extend and super
+
+
+// setTimeout( () => {
+//     console.log("hello")
+// }, 4000)
+
+let i = 0
+let id = setInterval(() => {
+    console.log(`${i}`)
+    i++
+}, 2000)
+
+setTimeout(() => {
+
+    clearInterval(id)
+
+}, 30000)
